0543f7270724f8067d54ed598729f144a4297381
apham727 <apham727@gmail.com>
2020-10-07 16:27:59 -0500

    vae sweep over alpha and dimensions

--- src/preprocess/dim_reduce/vae.py
+++ src/preprocess/dim_reduce/vae.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+import pickle
 
 import tensorflow as tf
 from tensorflow import keras
@@ -38,10 +39,11 @@ class Sampling(layers.Layer):
 
 
 class VAE(keras.Model):
-    def __init__(self, encoder, decoder, **kwargs):
+    def __init__(self, encoder, decoder, kl_loss_weight, **kwargs):
         super(VAE, self).__init__(**kwargs)
         self.encoder = encoder
         self.decoder = decoder
+        self.alpha = kl_loss_weight
 
     def train_step(self, data):
         if isinstance(data, tuple):
@@ -57,8 +59,7 @@ class VAE(keras.Model):
             )
 
             reconstruction_loss *= 1
-            alpha = 0
-            kl_loss = (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))*alpha
+            kl_loss = (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))*self.alpha
             kl_loss = tf.reduce_mean(kl_loss)
             kl_loss *= -0.5
             total_loss = reconstruction_loss + kl_loss
@@ -69,6 +70,50 @@ class VAE(keras.Model):
             "reconstruction_loss": reconstruction_loss,
             "kl_loss": kl_loss,
         }
+
+
+
+def train_vae(data, latent_dim, alpha, learning_rate, num_epoch):
+
+    # Build the encoder
+    encoder_inputs = keras.Input(shape=(100, 4))
+    x = layers.Flatten()(encoder_inputs)
+
+    x = layers.Dense(200, activation="linear", name="encode_layer_1")(x)
+    x = layers.Dense(100, activation="tanh", name="encode_layer_2")(x)
+    x = layers.Dense(50, activation="tanh", name="encode_layer_3")(x)
+    x = layers.Dense(25, activation="tanh", name="encode_layer_4")(x)
+    x = layers.Dense(10, activation="tanh", name="encode_layer_5")(x)
+
+    z_mean = layers.Dense(latent_dim, activation="tanh", name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, activation="tanh", name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder.summary()
+
+    # Build the decoder
+    latent_inputs = keras.Input(shape=(latent_dim,))
+
+    x = layers.Dense(10, activation="tanh", name="decode_layer_1")(latent_inputs)
+    x = layers.Dense(25, activation="tanh", name="decode_layer_2")(x)
+    x = layers.Dense(50, activation="tanh", name="decode_layer_3")(x)
+    x = layers.Dense(100, activation="tanh", name="decode_layer_4")(x)
+    x = layers.Dense(200, activation="tanh", name="decode_layer_5")(x)
+    x = layers.Dense(400, activation="linear", name="decode_layer_6")(x)
+
+    decoder_outputs = layers.Reshape((100, 4))(x)
+
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
+
+    vae = VAE(encoder, decoder, alpha)
+    vae.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate))
+
+    vaefit = vae.fit(data, epochs=num_epoch, batch_size=len(data))
+    return vae, vaefit
+
+
 
 
 """
@@ -84,161 +129,135 @@ learning_rate: rate of gradient descent, 0.01 is a good number
 """
 
 
-def run_vae(file_index, rng, plot_results=False):
+def vae_alpha_dim_sweep(file_index,  dim_rng, alpha_rng, learning_rate, num_epoch, save_results=False):
+    """
+    Performs a sweep across the alpha(KL-weight) and the dimensions range, and optionally calls a callback
+    function to execute optional code such as data splitting or plotting
+    :param file_index: the patient to perform the sweep over
+    :param dim_rng:
+    :param alpha_rng:
+    :param plot_results: True if function should plot the results
+    :return:
+    """
     # Load heartbeat data
     data = np.load(os.path.join("Working_Data", "Normalized_Fixed_Dim_HBs_Idx" + str(file_index) + ".npy"))
+    alpha_mses = {}
+    for alpha in alpha_rng:
+        dim_mses = {}
+        for latent_dim in dim_rng:
+            vae, vaefit = train_vae(data, latent_dim, 0.5, learning_rate, num_epoch)
 
-    splitting_idx = round(len(data)*5/6)
+            ###############################################################################################################
+            # PUT TRAIN DATA IN
+            # save the z parameter? save the z-mean or z-variance? --> YES
+            z = vae.encoder.predict(data)
+            reconstruction = vae.decoder.predict(z)
+
+            if save_results:
+                reconstruction_savename = os.path.join("Working_Data", "reconstructed_vaeAlpha{}_{}d_Idx{}.npy".format(alpha, latent_dim, file_index))
+                z_savename = os.path.join("Working_Data", "reduced_vae_" + str(latent_dim) + "d_Idx" + str(file_index) + ".npy")
+                np.save(reconstruction_savename, reconstruction)
+                np.save(z_savename, z)
+
+            # compute the mse between the original signal and the reconstruction
+            mse = np.zeros(np.shape(data)[0])
+            for i in range(np.shape(data)[0]):
+                mse[i] = (np.linalg.norm(data[i, :, :] - reconstruction[i, :, :]) ** 2) / (np.linalg.norm(data[i, :, :]) ** 2)
+
+            dim_mses[latent_dim] = mse.mean()
+        alpha_mses[alpha] = dim_mses
+
+    print(alpha_mses)
+    return alpha_mses
+
+
+
+def plot_data_splitting(data, vae, vaefit):
+    z = vae.encoder.predict(data)
+    reconstruction = vae.decoder.predict(z)
+    splitting_idx = round(len(data) * 5 / 6)
     data_train = data[0:splitting_idx]
-    data_test = data[(splitting_idx+1):]
+    data_test = data[(splitting_idx + 1):]
 
-    for latent_dim in rng:
-        num_epoch = 400
-        learning_rate = 0.001  # this is the default
+    # visualize the loss convergence as we iterate
+    plt.plot(vaefit.history['loss'])
+    plt.plot(vaefit.history['reconstruction_loss'])
+    plt.plot(vaefit.history['kl_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['loss', 'reconstruction loss', 'KL loss'], loc='upper left')
+    plt.show()
 
-        # Build the encoder
-        encoder_inputs = keras.Input(shape=(100, 4))
-        x = layers.Flatten()(encoder_inputs)
+    # htbt_idx = 15000
+    # # visualize the first heartbeat
+    # for lead_idx in [0, 1, 2, 3]:
+    #     plt.plot([i for i in range(len(data[htbt_idx, :, lead_idx]))], data[htbt_idx, :, lead_idx])
+    #     plt.plot([i for i in range(len(reconstruction[htbt_idx, :, lead_idx]))],
+    #              reconstruction[htbt_idx, :, lead_idx])
+    #     plt.legend(['original', 'reconstructed'], loc='upper left')
+    #     plt.title("VAE output comparison (heartbeat {}, lead {})".format(htbt_idx, lead_idx + 1))
+    #     plt.xlabel('Index')
+    #     plt.show()
 
-        x = layers.Dense(200, activation="linear", name="encode_layer_1")(x)
-        x = layers.Dense(100, activation="tanh", name="encode_layer_2")(x)
-        x = layers.Dense(50, activation="tanh", name="encode_layer_3")(x)
-        x = layers.Dense(25, activation="tanh", name="encode_layer_4")(x)
-        x = layers.Dense(10, activation="tanh", name="encode_layer_5")(x)
+    plt.figure()
+    data_stack = z[0];
+    plt.hist(data_stack, bins=np.linspace(-1.2, 1.2, num=501))
+    plt.title("Latent Variable Means - Train Data")
+    plt.show()
 
-        z_mean = layers.Dense(latent_dim, activation="tanh", name="z_mean")(x)
-        z_log_var = layers.Dense(latent_dim, activation="tanh", name="z_log_var")(x)
-        z = Sampling()([z_mean, z_log_var])
+    plt.figure()
+    data_stack = z[1];
+    plt.hist(data_stack, bins=np.linspace(-1.5, -0.5, num=501))
+    plt.title("Latent Variable log(Variance) - Train Data")
+    plt.show()
 
-        encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-        encoder.summary()
+    plt.figure()
+    data_stack = z[2];
+    plt.hist(data_stack, bins=np.linspace(-5, 5, num=101))
+    plt.title("Sampled Latent Variable - Train Data")
+    plt.show()
 
-        # Build the decoder
-        latent_inputs = keras.Input(shape=(latent_dim,))
+    ###############################################################################################################
+    # PUT TEST DATA IN
+    # save the z parameter? save the z-mean or z-variance? --> YES
+    z = vae.encoder.predict(data_test)
+    reconstruction = vae.decoder.predict(z)
+    # print(np.shape(z[2]))
 
-        x = layers.Dense(10, activation="tanh", name="decode_layer_1")(latent_inputs)
-        x = layers.Dense(25, activation="tanh", name="decode_layer_2")(x)
-        x = layers.Dense(50, activation="tanh", name="decode_layer_3")(x)
-        x = layers.Dense(100, activation="tanh", name="decode_layer_4")(x)
-        x = layers.Dense(200, activation="tanh", name="decode_layer_5")(x)
-        x = layers.Dense(400, activation="linear", name="decode_layer_6")(x)
+    # visualize the loss convergence as we iterate
+    plt.figure()
+    data_stack = z[0]
+    plt.hist(data_stack, bins=np.linspace(-1.2, 1.2, num=501))
+    plt.title("Latent Variable Means - Test Data")
+    plt.show()
 
-        decoder_outputs = layers.Reshape((100, 4))(x)
+    plt.figure()
+    data_stack = z[1]
+    plt.hist(data_stack, bins=np.linspace(-1.5, -0.5, num=501))
+    plt.title("Latent Variable log(Variance) - Test Data")
+    plt.show()
 
-
-        decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
-        decoder.summary()
-
-        vae = VAE(encoder, decoder)
-        vae.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate))
-
-        #print(np.shape(data))
-
-        vaefit = vae.fit(data_train, epochs=num_epoch, batch_size=len(data_train))
-
-        ###############################################################################################################
-        # PUT TRAIN DATA IN
-        # save the z parameter? save the z-mean or z-variance? --> YES
-        z = encoder.predict(data_train)
-        reconstruction = decoder.predict(z)
-        # print(np.shape(z[2]))
-
-        # visualize the loss convergence as we iterate
-        if plot_results:
-            plt.plot(vaefit.history['loss'])
-            plt.plot(vaefit.history['reconstruction_loss'])
-            plt.plot(vaefit.history['kl_loss'])
-            plt.title('model loss')
-            plt.ylabel('loss')
-            plt.xlabel('epoch')
-            plt.legend(['loss', 'reconstruction loss', 'KL loss'], loc='upper left')
-            plt.show()
-
-            htbt_idx = 15000
-            # visualize the first heartbeat
-            for lead_idx in [0, 1, 2, 3]:
-                plt.plot([i for i in range(len(data[htbt_idx, :, lead_idx]))], data[htbt_idx, :, lead_idx])
-                plt.plot([i for i in range(len(reconstruction[htbt_idx, :, lead_idx]))],
-                         reconstruction[htbt_idx, :, lead_idx])
-                plt.legend(['original', 'reconstructed'], loc='upper left')
-                plt.title("VAE output comparison (heartbeat {}, lead {})".format(htbt_idx, lead_idx + 1))
-                plt.xlabel('Index')
-                plt.show()
-
-            plt.figure()
-            data_stack = z[0];
-            plt.hist(data_stack, bins=np.linspace(-1.2, 1.2, num=501))
-            plt.title("Latent Variable Means - Train Data")
-            plt.show()
-
-            plt.figure()
-            data_stack = z[1];
-            plt.hist(data_stack, bins=np.linspace(-1.5, -0.5, num=501))
-            plt.title("Latent Variable log(Variance) - Train Data")
-            plt.show()
-
-            plt.figure()
-            data_stack = z[2];
-            plt.hist(data_stack, bins=np.linspace(-5, 5, num=101))
-            plt.title("Sampled Latent Variable - Train Data")
-            plt.show()
-
-        ###############################################################################################################
-        # PUT TEST DATA IN
-        # save the z parameter? save the z-mean or z-variance? --> YES
-        z = encoder.predict(data_test)
-        reconstruction = decoder.predict(z)
-        # print(np.shape(z[2]))
-
-        # visualize the loss convergence as we iterate
-        if plot_results:
-            plt.figure()
-            data_stack = z[0]
-            plt.hist(data_stack, bins=np.linspace(-1.2, 1.2, num=501))
-            plt.title("Latent Variable Means - Test Data")
-            plt.show()
-
-            plt.figure()
-            data_stack = z[1]
-            plt.hist(data_stack, bins=np.linspace(-1.5, -0.5, num=501))
-            plt.title("Latent Variable log(Variance) - Test Data")
-            plt.show()
-
-            plt.figure()
-            data_stack = z[2]
-            plt.hist(data_stack, bins=np.linspace(-5, 5, num=101))
-            plt.title("Sampled Latent Variable - Test Data")
-            plt.show()
+    plt.figure()
+    data_stack = z[2]
+    plt.hist(data_stack, bins=np.linspace(-5, 5, num=101))
+    plt.title("Sampled Latent Variable - Test Data")
+    plt.show()
 
 
-        log_filepath = os.path.join("Working_Data", "")
-        os.makedirs(os.path.dirname(log_filepath), exist_ok=True)
 
-        reconstruction_savename = os.path.join("Working_Data", "reconstructed_vae_" + str(latent_dim) + "d_Idx" + str(
-            file_index) + ".npy")
-        z_savename = os.path.join("Working_Data", "reduced_vae_" + str(latent_dim) + "d_Idx" + str(file_index) + ".npy")
-        np.save(reconstruction_savename, reconstruction)
-        np.save(z_savename, z)
 
 
 if __name__ == "__main__":
-    # threads = []
-    # for file_index in heartbeat_split.indicies[:5]:
-    #     t = threading.Thread(target=run_vae, args=(10, False))
-    #     t.start()
-    #     threads.append(t)
-    #
-    # for thread in threads:
-    #     thread.join()
-    #
-    # threads = []
-    # for file_index in heartbeat_split.indicies[5:10]:
-    #     t = threading.Thread(target=run_vae, args=(10, False))
-    #     t.start()
-    #     threads.append(t)
-    #
-    # for thread in threads:
-    #     thread.join()
+    patient_mses = {}
+    for file_index in heartbeat_split.indicies[:10]:
+        patient_mses[file_index] = vae_alpha_dim_sweep(file_index, range(1, 11), [1, 0.5, 0.1, 0.05, 0.001, 0], 0.001, 1000, save_results=True)
 
-    for file_index in heartbeat_split.indicies[:1]:
-        run_vae(file_index, range(1, 2), plot_results=True)
+    outfile = open("Working_Data/vae_sweep_mses.pkl", 'wb')
+    pickle.dump(patient_mses, outfile)
+    outfile.close()
+
+
+    # if we want to perform data splitting across a smaller dimension range:
+    # for file_index in heartbeat_split.indicies[:1]:
+    #     vae_alpha_dim_sweep(file_index, range(1, 4), [1, 0.5, 0.1, 0.05, 0.001, 0], 0.001, 1000, custom_callback=plot_data_splitting)
