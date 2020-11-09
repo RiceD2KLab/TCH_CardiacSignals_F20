de05e6dbc463c95efc70f6cccda5ec71c916e207
kunalrye <ksrai99@gmail.com>
2020-11-08 21:02:16 -0600

    added prelim CAE architecture to file, pending testing

--- src/preprocess/dim_reduce/autoencoder.py
+++ src/preprocess/dim_reduce/autoencoder.py
@@ -8,7 +8,7 @@ import os
 import threading
 from tensorflow import keras
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Dense, Flatten, Reshape, Input, InputLayer, Dropout
+from tensorflow.keras.layers import Dense, Flatten, Reshape, Input, InputLayer, Conv1D, MaxPooling1D, BatchNormalization, UpSampling1D
 from tensorflow.keras.models import Sequential, Model
 from src.preprocess.dim_reduce.patient_split import *
 
@@ -67,6 +67,67 @@ def build_autoencoder(sig_shape, encode_size):
     return encoder, decoder
 
 
+def build_convolutional_autoencoder(sig_shape, encode_size):
+    """
+
+    :param sig_shape:
+    :param encode_size:
+    :return:
+    """
+    latent_dim = 10
+    # Build the encoder
+    encoder_inputs = keras.Input(shape=(100, 4))
+    encoder = Sequential()
+    encoder.add(InputLayer((100, 4)))
+
+    encoder.add(Conv1D(32, 3, activation="linear", padding="same"))
+    encoder.add(MaxPooling1D(2, padding="same"))
+
+    encoder.add(Conv1D(64, 3, activation="relu", padding="same"))
+    encoder.add(MaxPooling1D(1, padding="same"))
+
+    encoder.add(BatchNormalization())
+
+    encoder.add(Conv1D(128, 3, activation="relu", padding="same"))
+    encoder.add(Conv1D(64, 3, activation="relu", padding="same"))
+    encoder.add(MaxPooling1D(5, padding="same"))
+
+    encoder.add(BatchNormalization())
+    encoder.add(Conv1D(32, 3, activation="relu", padding="same"))
+    encoder.add(Conv1D(16, 3, activation="relu", padding="same"))
+    encoder.add(Conv1D(1, 3, activation="relu", padding="same"))
+
+    encoder.summary()
+    ####################################################################################################################
+    # Build the decoder
+    # %%
+
+    decoder = Sequential()
+    decoder.add(InputLayer((latent_dim, 1)))
+    # decoder.add(UpSampling1D(2))
+    decoder.add(Conv1D(1, 7, activation="relu", padding="same"))
+    decoder.add(Conv1D(16, 3, activation="relu", padding="same"))
+    decoder.add(UpSampling1D(1))
+
+    decoder.add(Conv1D(32, 7, activation="relu", padding="same"))
+    decoder.add(Conv1D(64, 9, activation="relu", padding="same"))
+    decoder.add(UpSampling1D(2))
+
+    decoder.add(Conv1D(32, 5, activation="relu", padding="same"))
+    decoder.add(Conv1D(16, 3, activation="relu", padding="same"))
+    decoder.add(UpSampling1D(5))
+
+    decoder.add(Conv1D(4, 9, activation="relu", padding="same"))
+    # decoder.add(UpSampling1D(2))
+    # decoder.add(Conv1D(4, 7, activation="tanh", padding="same"))
+
+    # decoder.add(UpSampling1D(2))
+    # decoder.add(Conv1D(4, 3, activation="tanh", padding="same"))
+
+    decoder.summary()
+    return encoder, decoder
+
+
 def training_ae(num_epochs, reduced_dim, file_index):
     """
     Training function for deterministic autoencoder model, saves the encoded and reconstructed arrays
@@ -87,12 +148,13 @@ def training_ae(num_epochs, reduced_dim, file_index):
     autoencoder = Model(inp, reconstruction)
     autoencoder.compile(optimizer='Adam', loss='mse')
 
-    early_stopping = EarlyStopping(patience=5)
-    autoencoder.fit(x=normal, y=normal, epochs=num_epochs, validation_split=0.3, callbacks=early_stopping)
+    early_stopping = EarlyStopping(patience=20, min_delta=0.001, mode='min')
+    autoencoder.fit(x=normal, y=normal, epochs=num_epochs, validation_split=0.25, callbacks=early_stopping)
 
     # save out the model
-    # filename = 'ae_patient_' + str(file_index) + '_dim' + str(reduced_dim)
-    # autoencoder.save(filename + '.h5')
+    filename = 'ae_patient_' + str(file_index) + '_dim' + str(reduced_dim) + '_model'
+    autoencoder.save(filename + '.h5')
+    print('Model saved for ' + 'patient ' + str(file_index))
 
     # using AE to encode other data
     encoded = encoder.predict(full)
@@ -106,15 +168,16 @@ def training_ae(num_epochs, reduced_dim, file_index):
     # np.save(input_save, normal_test)
 
 
-def run_over(num_epochs, encoded_dim):
+def run(num_epochs, encoded_dim):
     """
     Run training autoencoder over all dims in list
     :param num_epochs: number of epochs to train for
     :param encoded_dim: dimension to run on
     :return None, saves arrays for reconstructed and dim reduced arrays
     """
-    indicies = ['30', '31', '32',
-                '33', '34', '35', '37', '38', '39']
+    indicies = ["33"]
+    # indicies = ['30', '31', '32',
+    #             '33', '34', '35', '37', '38', '39']
 
     for patient_ in indicies:
         print("Starting on index: " + str(patient_))
@@ -126,4 +189,4 @@ def run_over(num_epochs, encoded_dim):
 #################### Training to be done for 100 epochs for all dimensions ############################################
 # for i in range(5, 20):
 #     run_over(100, i)
-# run_over(300, 14)
+# run(100, 10)
