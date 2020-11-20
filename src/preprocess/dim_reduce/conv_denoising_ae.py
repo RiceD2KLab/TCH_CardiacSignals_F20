166769a79d99951b6de35fd3640113d700176b66
Kunal Rai <ksrai99@gmail.com>
2020-11-20 15:30:49 -0600

    Update conv_denoising_ae.py

--- src/preprocess/dim_reduce/conv_denoising_ae.py
+++ src/preprocess/dim_reduce/conv_denoising_ae.py
@@ -115,7 +115,7 @@ def training_ae(num_epochs, reduced_dim, file_index):
     signal_shape = normal.shape[1:]
     batch_size = round(len(normal) * 0.15)
     # try using the first hour and second hour for train and validation and see if there are any differences
-    
+
     encoder, decoder = build_model(signal_shape, reduced_dim)
 
     inp = Input(signal_shape)
