6d82dcb6a027b9a176debd054dc3d5a27d1cecf6
chiraag <cvk4@rice.edu>
2021-01-24 22:08:22 -0600

    find confidence interval of mean earliness

--- src/models/changepoint.py
+++ src/models/changepoint.py
@@ -5,10 +5,12 @@ Also contains code for all CUSUM plots shown in our report.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from src.utils.file_indexer import get_patient_ids 
 from src.utils.dsp_utils import get_windowed_time
 from src.utils.plotting_utils import set_font_size
 from src.models.mse import mean_squared_error
+from scipy.stats import sem
 import os
 
 def cusum(patient, model_name, dimension):
@@ -38,12 +40,14 @@ def cusum(patient, model_name, dimension):
         cusum.append(max(cusum[-1] + L, 0))
     cusum = cusum[1:]
 
-    set_font_size()
-    plt.plot(time_stamps[len(time_stamps)//3:], cusum[len(time_stamps)//3:])
-    plt.title(f"Individual Patient: CUSUM statistic over time")
-    plt.xlabel("Time before cardiac arrest (hours)")
-    plt.ylabel("CUSUM Score")
-    plt.savefig('Working_Data/cusum_single_patient.png', dpi=500)
+    # set_font_size()
+    # rcParams.update({'figure.autolayout': True})
+
+    # plt.plot(time_stamps[len(time_stamps)//3:], cusum[len(time_stamps)//3:])
+    # plt.title(f"Individual Patient: CUSUM statistic over time")
+    # plt.xlabel("Time before cardiac arrest (hours)")
+    # plt.ylabel("CUSUM Score")
+    # plt.savefig('images/cusum_single_patient.png', dpi=500)
 
     # plt.show()
     filename = os.path.join("Working_Data", f"unwindowed_cusum_100d_Idx{patient}.npy")
@@ -59,7 +63,8 @@ def cusum_validation(threshold):
     """
     count = 0 # number of patients with cusum > threshold before cardiac arrest
     total = 0 # total valid patients
-    avg_time = 0 # average detection time
+    detection_times = [] # detection time for each valid patient
+    # avg_time = 0 # average detection time
     for idx in get_patient_ids():
         try:
             cusum_vals = np.load(f"Working_Data/unwindowed_cusum_100d_Idx{idx}.npy") # load cusum scores for this patient
@@ -69,21 +74,24 @@ def cusum_validation(threshold):
                 stop_time = patient_times[stop_index]
                 if stop_index > len(cusum_vals)/2: # make sure change is in last three hours of data (no false positives)
                     count += 1
-                    avg_time += stop_time
+                    detection_times.append(stop_time)
+                    # avg_time += stop_time
                 total += 1
         except:
             print("No File Found: Patient " + idx)
             continue
-    avg_time /= count
+    # avg_time /= count
+    avg_time = np.mean(detection_times)
+    sem_time = sem(detection_times)
     print(f"Threshold crossed within final 3 hours: {count}/{total}")
-    print(f"Average Detection Earliness: {avg_time} hours")
-    return count, total, avg_time
+    print(f"Average Detection Earliness: {avg_time} +- {sem_time} hours")
+    return count, total, avg_time, sem_time
 
-def cusum_box_plot(indices, model_name, dimension):
+def cusum_box_plot(patient_indices, model_name, dimension):
     """
     Generates figure of boxplots over time of CUSUM scores across the entire patient cohort
 
-    :param indices: [list(int)] list of patient identifiers to use
+    :param patient_indices: [list(int)] list of patient identifiers to use
     :param model_name: [string] model type used for the desired data (typically "cdae")
     :param dimension: [int] dimension used for reduction in the above model (typically 100)
     :return: None (saves plot of CUSUM distributions over time)
@@ -92,30 +100,41 @@ def cusum_box_plot(indices, model_name, dimension):
     cusum_values = []
     window_times = np.linspace(-4, 0, 49) # use 50 windows
 
-    for idx in indices:
+    for idx in patient_indices:
         try:
             current_values = np.load(f"Working_Data/unwindowed_cusum_100d_Idx{idx}.npy") # load anomaly rates for this patient
+
+            if len(current_values) < 1000:
+                raise e
+
             patient_times = get_windowed_time(idx, num_hbs=10, window_size=1)[:-1]
-
+            # print(len(current_values))
+            # print(len(patient_times))
             test_values = []
-
             for i in range(len(window_times) - 1):
                 indices = np.squeeze(np.argwhere(np.logical_and(patient_times >= window_times[i], patient_times < window_times[i+1]))) # indices in this window
+                indices = indices[indices < len(current_values)]
+
                 if len(indices) == 0:
                     test_values.append(None) # marker for no data available in this window
                 else:
                     window_scores = current_values[indices] # cusum scores for all data points found in current window
                     test_values.append(np.mean(window_scores))
                     # print(test_values[-1])
+                # print(len(test_values))
             cusum_values.append(test_values)
         except:
-            print("No File Found: Patient " + idx)
+            print("Insufficient Data: Patient " + idx)
             continue
+        
+
     cusum_values = np.array(cusum_values).T.tolist()
     for row in cusum_values: # remove "None" from windows where no data found
         while None in row: row.remove(None)
 
     set_font_size()
+    rcParams.update({'figure.autolayout': True})
+
     plt.boxplot(cusum_values, showfliers=False, positions=window_times[:-1], widths=1/15,
         medianprops=dict(color='red', linewidth=2.5), whiskerprops=dict(color='lightgrey'), capprops=dict(color='lightgrey'), boxprops=dict(color='lightgrey'))
     
@@ -124,13 +143,16 @@ def cusum_box_plot(indices, model_name, dimension):
     plt.ylabel("CUSUM Score")
     plt.xticks(np.arange(-4, 1, 1), np.arange(-4, 1, 1))
     plt.xlim(-4.2, 0.2)
-    plt.savefig('images/cusum_boxplot.png', dpi=500)
-    # plt.show()
-
+    # plt.savefig('images/cusum_boxplot.png', dpi=500)
+    plt.show()
 
 if __name__ == "__main__":
     # Uncomment the below two lines to reproduce the figures from the report
 
     # cusum_box_plot(get_patient_ids(), "cdae", 100)
+    # for idx in get_patient_ids():
+    #     cusum(idx, "cdae", dimension=100)
     # cusum(16, "cdae", dimension=100)
+    cusum_validation(500)
+
     pass
