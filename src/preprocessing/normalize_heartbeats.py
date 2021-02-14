213577d4623a652d5371467d6d5f2df56884b0d2
Andrew Pham <apham727@gmail.com>
2021-02-14 15:36:48 -0600

    noramlize conterol group

--- src/preprocessing/normalize_heartbeats.py
+++ src/preprocessing/normalize_heartbeats.py
@@ -11,18 +11,20 @@ from src.preprocessing import heartbeat_split
 from sklearn.preprocessing import StandardScaler
 from src.utils.file_indexer import get_patient_ids
 
-def normalize_heartbeats():
+def normalize_heartbeats(control=False):
     """
     Normalizes the Fixed_Dims_HBs 3D matrices (shape n x 100 x 4) by normalizing each 100x4 matrix to mean 0, variance 1
     :return: nothing, saves the normalized heartbeats to a Normalized_Fixed_Dims_HBs_Idx{k}.npy file
     """
-    for file_index in get_patient_ids():
+    for file_index in get_patient_ids(control):
         try:
-            original_signals = np.load(os.path.join("Working_Data", "Fixed_Dim_HBs_Idx{}.npy".format(file_index)))
+            working_dir = "Control_Working_Data" if control else "Working_Data"
+
+            original_signals = np.load(os.path.join(working_dir, "Fixed_Dim_HBs_Idx{}.npy".format(file_index)))
             for i in range(np.shape(original_signals)[0]):
                 original_signals[i, :,:] = StandardScaler().fit_transform(original_signals[i,:,:])
 
-            np.save(os.path.join("Working_Data", "Normalized_Fixed_Dim_HBs_Idx{}.npy".format(file_index)), original_signals)
+            np.save(os.path.join(working_dir, "Normalized_Fixed_Dim_HBs_Idx{}.npy".format(file_index)), original_signals)
             print("Normalized patient {}".format(file_index))
         except:
             pass
